8284f73ec25a1975c069c3a3d81a68ec5833b580
harsh <htandel@horizontal.com>
2026-02-04 16:25:21 +0530

    schema designs

--- config/schema.tsx
+++ config/schema.tsx
@@ -1,4 +1,5 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+import { integer, pgTable, varchar,date } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,3 +7,12 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
   credits:integer().default(10),
 });
+
+export const projectTable = pgTable("projects", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  projectId: varchar({ length: 255 }).notNull().unique(),
+  userInput : varchar(),
+  deviceInput : varchar(),
+  createdAt: date().defaultNow(),
+  userId: varchar().references(()=>usersTable.email).notNull(),
+})
